ff856e029ee088672d3cd5cf77140e39ef2c7c26
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2019-02-28 17:02:36 +0300

    change tronbox to truffle

--- scripts/templated-tests/compile-template.sh
+++ scripts/templated-tests/compile-template.sh
@@ -3,5 +3,5 @@ set -e
 rm -rf build
 rm -f contracts/LostKeyMain.sol
 source $(dirname "$0")/templated-tests/preprocess-template.sh $1
-node_modules/.bin/tronbox compile --all
+node_modules/.bin/truffle compile --all
 node_modules/.bin/combine-contracts LostKeyMain
